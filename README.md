a225ec9566b1871894acd33cfb98a531564a1700
bloodor <33715536+bloodor@users.noreply.github.com>
2023-07-24 11:51:41 +0200

    make it have an UI fully working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-scissors
+
+Image by <a href="https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=296853">Clker-Free-Vector-Images</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=296853">Pixabay</a>
+
+Image by <a href="https://pixabay.com/users/thedigitalartist-202249/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1657428">Pete Linforth</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1657428">Pixabay</a>
--- rps.js
+++ rps.js
@@ -1,3 +1,31 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let playerButtons = document.querySelectorAll('.playerButton');
+let resultText = document.querySelector('#result');
+let computerScoreText = document.querySelector('#computerScore');
+let playerScoreText = document.querySelector('#playerScore');
+
+playerButtons.forEach(element => element.addEventListener('click', function() {
+    playRound(this.id);
+
+    playerScoreText.textContent = `Player score: ${playerScore}`;
+    computerScoreText.textContent = `Computer score: ${computerScore}`;
+    
+    if (playerScore === 5) {
+        resultText.textContent = "Congratulations! You won the game.";
+    }
+    else if (computerScore === 5) {
+        resultText.textContent =  "Better luck next time! Computer won the game.";
+    }
+    
+    if (computerScore === 5 || playerScore === 5) {
+        computerScore = 0;
+        playerScore = 0;
+    }
+}));
+
+
 getComputerChoice = () =>  {
     let value = "";
     let number = Math.floor(Math.random() * (3));;
@@ -17,27 +45,21 @@ getComputerChoice = () =>  {
 
 win = (player, computer) => {
     playerScore++;
-    return `You won this round, ${player} beats ${computer}`;
+    resultText.textContent = `You won this round, ${player} beats ${computer}`;
 }
 
-draw = (player, computer) => {
-    return `This round is a tie ! You both chose ${player}`;
+draw = (player) => {
+    resultText.textContent = `This round is a tie ! You both chose ${player}`
 }
 
 loss = (player, computer) => {
     computerScore++;
-    return `You lost this round, ${player} gets beaten by ${computer}`;
+    resultText.textContent = `You lost this round, ${player} gets beaten by ${computer}`;
 }
 
-unexpectedResult = () => {
-    return "Please enter rock, paper or scissors (case do not matter)";
-}
-
-
-playRound = (playerSelection, computerSelection) => {
-    if (playerSelection === '')
-        return unexpectedResult();
-    else if (playerSelection === computerSelection)
+playRound = (playerSelection) => {
+    const computerSelection = getComputerChoice();
+    if (playerSelection === computerSelection)
         return draw(playerSelection);
     else if (playerSelection === 'rock') {
         if (computerSelection === 'paper')
@@ -56,20 +78,3 @@ playRound = (playerSelection, computerSelection) => {
             return win(playerSelection, computerSelection);
     }
 }
-
-let playerScore = 0;
-let computerScore = 0;
-
-game = () => {
-    while (playerScore < 3 && computerScore < 3) {
-        let computerChoice = getComputerChoice();
-        let userChoice = prompt("Rock, Paper or Scissors ?").toLowerCase();
-        console.log(playRound(userChoice, computerChoice));
-    }
-    if (playerScore === 3)
-        console.log("Congratulations! You won the game.")
-    else if (computerScore === 3)
-        console.log("You will do better next time! Computer won the game.")
-}
-
-game();
